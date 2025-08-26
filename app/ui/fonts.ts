01d71807592ca6dad869b1e15ae14efd370accfe
Haiver <haiverhercas@gmail.com>
2025-08-25 23:08:43 -0500

    add font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Lusitana } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
