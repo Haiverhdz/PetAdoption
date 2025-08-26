c1549e88835b50731156c743a84dbd8d3e7d99d2
Haiver <haiverhercas@gmail.com>
2025-08-25 23:03:12 -0500

    correccion

--- app/middlewares/auth.ts
+++ app/middlewares/auth.ts
@@ -1,10 +1,28 @@
 import passport from "../lib/passport";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const authenticateJWT = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
-  passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ message: "No autorizado" });
-    req.user = user;
-    next();
-  })(req, res);
+interface JwtUser {
+  id: string;
+  email: string;
+  name: string;
+}
+
+interface NextApiRequestWithUser extends NextApiRequest {
+  user?: JwtUser;
+}
+
+export const authenticateJWT = (
+  req: NextApiRequestWithUser,
+  res: NextApiResponse,
+  next: () => void
+) => {
+  passport.authenticate(
+    "jwt",
+    { session: false },
+    (err: any, user: JwtUser | false) => {
+      if (err || !user) return res.status(401).json({ message: "No autorizado" });
+      req.user = user;
+      next();
+    }
+  )(req, res, next);
 };
